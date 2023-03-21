108fa4f0a177d98bd07b5b01b50d0a7d290b2c62
JeffK@Anthology <jeff.kelley@anthology.com>
2023-03-21 15:32:17 -0400

    Update gradecenter_use_20230317.sql
    comments and clarifications

--- gradecenter_use_20230317.sql
+++ gradecenter_use_20230317.sql
@@ -1,3 +1,9 @@
+-- jeff.kelley@anthology.com  March 2023
+-- provided without support or warranty
+
+-- for a group of courses, provide instructors, time spent grading, count of students, grade items and grades.
+
+
 SELECT 
   crs.course_number AS course_id
   ,per.stage['user_id']::text AS instructor_id
@@ -11,19 +17,19 @@ FROM course crs
   LEFT JOIN person_course pc on pc.course_id = crs.id
   LEFT JOIN person per on per.id = pc.person_id
 
-  --student counter 
+  --student counter subquery
   LEFT JOIN (
     SELECT
       course_id,
       count(1) as student_count
     FROM person_course
-    WHERE row_deleted_time IS NULL
-      AND course_role = 'S'
-      AND enabled_ind
+    WHERE row_deleted_time IS NULL --no deleted enrollments
+      AND course_role = 'S'        --student role only
+      AND enabled_ind              --no disabled enrollments
     GROUP BY course_id
     ) std ON std.course_id = crs.id
 
-  -- item counter  
+  -- gradebok item counter subquery 
   LEFT JOIN ( 
     SELECT 
       course_id,
@@ -34,7 +40,7 @@ FROM course crs
     GROUP BY course_id      
       ) gbk ON gbk.course_id = crs.id
 
-  --grades counter
+  --grades counter subquery
   LEFT JOIN (
     SELECT 
       gradebook.course_id,
@@ -46,7 +52,7 @@ FROM course crs
     GROUP BY gradebook.course_id      
       ) gds ON gds.course_id = crs.id
 
-  --grading activity by instuctor
+  --grading activity by instuctor subquery
   LEFT JOIN (
     SELECT 
       person_course_id,
@@ -57,9 +63,9 @@ FROM course crs
     GROUP BY person_course_id
     ) gact ON gact.person_course_id = pc.id
 
-WHERE pc.course_role = 'I'
-  AND pc.row_deleted_time IS NULL
-  AND crs.course_number like '%kelley%'
-  AND crs.row_deleted_time IS NULL
+WHERE pc.course_role = 'I'            -- only course instructors
+  AND pc.row_deleted_time IS NULL     -- no deleted course members
+  AND crs.course_number like '%%'     -- Learn course_id filter
+  AND crs.row_deleted_time IS NULL    -- no deleted courses
 
 ORDER BY crs.course_number ASC
