b304dbc1ff24796fb1f5d4c22fcbed4fbc4ee45b
JeffK@Anthology <jeff.kelley@anthology.com>
2026-02-20 15:40:41 -0500

    Update ultra_activity_audit_example_courseConvert.sql

--- SQL/ultra_activity_audit_example_courseConvert.sql
+++ SQL/ultra_activity_audit_example_courseConvert.sql
@@ -1,7 +1,7 @@
 -- find out who converted a course to ultra
 -- example of useing the objectId value from Ultra Events
 -- for audit activity investigations.
--- jeff.kelley@anthology.com  2025-08-09
+-- jeff.kelley@anthology.com  updated 2026-02-20
 -- no warranty or support
 
 
@@ -11,13 +11,14 @@ SELECT
   ue.data:objectId::text AS objectId,
   ue.event_type,
   ue.event_time,
-  --ue.data::text   --examine the full data object
+  --ue.data::text   -- uncomment to examine the full data object
 FROM CDM_TLM.ultra_events ue
 JOIN CDM_LMS.person per
   ON per.stage:uuid::text = ue.data:userId::text
 JOIN CDM_LMS.course cor
   ON '_' || cor.source_id || '_1' = ue.data:contextId::text
-WHERE cor.course_number like '%'   --select course(s)
-  AND ue.event_time BETWEEN '2025-07-01' AND '2025-08-31'
-  AND ue.data:objectId::text = 'course.conversion.statusBar.useUltra.button'  -- this is the analytics_id tag for the button
+WHERE cor.course_number like '%'   --select course(s) by Blackboard course id
+  AND per.stage:user_id::text like '%'   -- select user(s) by Blackboard user id
+  AND ue.event_time BETWEEN '2026-02-01' AND '2026-02-20'  --select timeframe
+  AND ue.data:objectId::text = 'course.conversion.statusBar.useUltra.button'  -- select the analytics_id tag for the button
 ORDER BY ue.event_time DESC;
